e12ae95a473df5be67ef95d571526c37d5818c56
CJ McAllister <cjm571@gmail.com>
2017-11-25 03:55:19 +0000

    Added print_stats function. Minor changes to accommodate new Ability design

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,49 @@ extern crate cast_iron;
 
 use cast_iron::actor::Actor;
 use cast_iron::ability::Ability;
-use cast_iron::ability::aspect;
+use cast_iron::ability::aspect::*;
+
+
+///////////////////////////////////////////////////////////////////////////////
+//  Functions and Methods
+///////////////////////////////////////////////////////////////////////////////
+
+// Outputs the stats of the given player to the terminal
+fn print_stats(actor: Actor) {
+    println!("BEGIN Stats for actor {}", actor.get_name());
+
+    let pos = actor.get_pos();
+    println!(" - Position: ({}, {}, {})", pos[0], pos[1], pos[2]);
+
+    println!(" - Fatigue: Current: {}", actor.get_cur_fatigue());
+
+    println!(" - Abilities:");
+    let mut i = 1;
+    for abil in actor.get_abilities() {
+        println!("   {}: {} - Potency {}", i, abil.get_name(), abil.get_potency());
+        for key in abil.get_aspects().keys() {
+            println!("      {:?}: IDK", key);
+            //TODO: Union was a poor choice... look into example at https://stackoverflow.com/a/29249230
+        }
+        i = i + 1
+    }
+
+    println!("END Stats for actor {}", actor.get_name());
+}
 
 fn main() {
     let mut lightning_bolt: Ability = Ability::new();
-    lightning_bolt.set_aesthetics(aspect::Aesthetics::Impressive);
-    lightning_bolt.set_element(aspect::Element::Electric);
-    lightning_bolt.set_method(aspect::Method::Wand);
-    lightning_bolt.set_morality(aspect::Morality::Neutral);
-    lightning_bolt.set_school(aspect::School::Destruction);
+    lightning_bolt.set_name("Lightning Bolt");
+    lightning_bolt.set_aspect(Tag::Aesthetics, Value {aesthetics: Aesthetics::Impressive});
+    lightning_bolt.set_aspect(Tag::Element, Value {element: Element::Electric});
+    lightning_bolt.set_aspect(Tag::Method, Value {method: Method::Wand});
+    lightning_bolt.set_aspect(Tag::Morality, Value {morality: Morality::Neutral});
+    lightning_bolt.set_aspect(Tag::School, Value {school: School::Destruction});
 
     let mut player_one: Actor = Actor::new();
+    player_one.set_name("CJ McAllister");
     player_one.add_ability(lightning_bolt);
+
+    print_stats(player_one);
 }
+
