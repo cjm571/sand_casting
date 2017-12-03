7bedc9ae893d910c9af5048c0498e04534425456
CJ McAllister <cjm571@gmail.com>
2017-12-03 14:28:35 +0000

    Adapted to new Aspects implementation

--- src/main.rs
+++ src/main.rs
@@ -19,36 +19,45 @@ fn print_stats(actor: Actor) {
     println!("BEGIN Stats for actor {}", actor.get_name());
 
     let pos = actor.get_pos();
-    println!(" - Position: ({}, {}, {})", pos[0], pos[1], pos[2]);
+    println!("Position: ({}, {}, {})", pos[0], pos[1], pos[2]);
 
-    println!(" - Fatigue: Current: {}", actor.get_cur_fatigue());
+    println!("Fatigue: Current: {}", actor.get_cur_fatigue());
 
-    println!(" - Abilities:");
+    println!("Abilities:");
     let mut i = 1;
     for abil in actor.get_abilities() {
-        println!("   {}: {} - Potency {}", i, abil.get_name(), abil.get_potency());
-        for key in abil.get_aspects().keys() {
-            println!("      {:?}: IDK", key);
-            //TODO: Union was a poor choice... look into example at https://stackoverflow.com/a/29249230
-        }
-        i = i + 1
+        println!("{}: Name:    {}", i, abil.get_name());
+        println!("   Potency: {}", abil.get_potency());
+        println!("   {:?}", abil.get_aspects());
+        i = i + 1;
     }
 
     println!("END Stats for actor {}", actor.get_name());
 }
 
 fn main() {
-    let mut lightning_bolt: Ability = Ability::new();
-    lightning_bolt.set_name("Lightning Bolt");
-    lightning_bolt.set_aspect(Tag::Aesthetics, Value {aesthetics: Aesthetics::Impressive});
-    lightning_bolt.set_aspect(Tag::Element, Value {element: Element::Electric});
-    lightning_bolt.set_aspect(Tag::Method, Value {method: Method::Wand});
-    lightning_bolt.set_aspect(Tag::Morality, Value {morality: Morality::Neutral});
-    lightning_bolt.set_aspect(Tag::School, Value {school: School::Destruction});
+    let null_abil: Ability = Ability::new("Null");
 
-    let mut player_one: Actor = Actor::new();
-    player_one.set_name("CJ McAllister");
+    let mut lightning_bolt: Ability = Ability::new("Lightning Bolt");
+    lightning_bolt.set_potency(20);
+    lightning_bolt.set_aesthetics(Aesthetics::Impressive);
+    lightning_bolt.set_element(Element::Electric);
+    lightning_bolt.set_method(Method::Wand);
+    lightning_bolt.set_morality(Morality::Neutral);
+    lightning_bolt.set_school(School::Destruction);
+
+    let mut blood_drain: Ability = Ability::new("Blood Drain");
+    blood_drain.set_potency(50);
+    blood_drain.set_aesthetics(Aesthetics::Ugly);
+    blood_drain.set_element(Element::Dark);
+    blood_drain.set_method(Method::Manual);
+    blood_drain.set_morality(Morality::Evil);
+    blood_drain.set_school(School::Destruction);
+
+    let mut player_one: Actor = Actor::new("CJ McAllister");
     player_one.add_ability(lightning_bolt);
+    player_one.add_ability(blood_drain);
+    player_one.add_ability(null_abil);
 
     print_stats(player_one);
 }
